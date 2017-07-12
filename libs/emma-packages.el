42fd8c0a7d9064306853e31970d69bde3c7eb8b7
Gabe Lievano <gabe@jglievano.me>
2017-07-12 15:44:40 -0700

    Adds magit.

--- libs/emma-packages.el
+++ libs/emma-packages.el
@@ -2,14 +2,18 @@
   (add-to-list 'load-path (expand-file-name dep emma-vendor)))
 
 (add-to-list 'load-path (expand-file-name "vendor" user-emacs-directory))
+(load-vendor-dep "dash")
 (load-vendor-dep "diminish")
 (load-vendor-dep "use-package")
+(load-vendor-dep "with-editor")
 
 (eval-when-compile
   (require 'use-package))
+(require 'dash)
 (require 'delight)
 (require 'diminish)
 (require 'bind-key)
+(require 'with-editor)
 
 ;; flx.
 (use-package flx
@@ -53,7 +57,17 @@
   (evil-leader/set-key
     "pf" 'projectile-find-file))
 
-(require 'projectile)
-(projectile-mode)
+;; magit.
+(use-package magit
+  :load-path "vendor/magit/lisp"
+  :init
+  (require 'magit)
+  (with-eval-after-load 'info
+    (info-initialize)
+    (add-to-list 'Info-directory-list
+		 "~/.emacs.d/vendor/magit/Documentation/"))
+  (which-key-add-key-based-replacements "<SPC> g" "git")
+  (evil-leader/set-key
+    "gs" 'magit-status))
 
 (provide 'emma-packages)
