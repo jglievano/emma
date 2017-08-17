7144d6dd712739b4023548f9cee70e9bb11da2f6
Gabe Lievano <gabe@jglievano.me>
2017-08-16 19:38:49 -0700

    Everything is now in emma.org

--- init.el
+++ init.el
@@ -21,39 +21,10 @@
   (warn "org-mode is out of date. org-mode >= 9 expected got %s instead"
 	(org-version)))
 
-;; No bell.
-(setq visible-bell 1)
-
-;; No startup or splash gimmicks
-(setq inhibit-splash-screen t)
-(setq inhibit-startup-message t)
-
-;; 2-space indentation
-(setq indent-tabs-mode nil)
-(setq tab-width 2)
-(setq-default c-basic-offset 2)
-(setq-default css-indent-offset 2)
-
-;; whitespace-mode
-(require 'whitespace)
-
-;; better backup
-(setq backup-directory-alist `(("." . "~/.saves")))
-(setq backup-by-copying t
-      delete-old-versions 6
-      kept-old-versions 2
-      version-control t)
-
 (mapc #'(lambda (path)
           (add-to-list 'load-path
                        (expand-file-name path user-emacs-directory)))
       '("lisp" "vendor/use-package"))
-
-;; lisp
-(autoload 'enable-paredit-mode "paredit"
-  "Turn on pseudo-structural editing on Lisp code." t)
-(add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
-(add-hook 'lisp-mode-hook #'enable-paredit-mode)
 
 ;; org-mode
 (global-set-key "\C-cl" 'org-store-link)
@@ -68,8 +39,6 @@
 (if (and (fboundp 'server-running-p)
          (not (server-running-p)))
     (server-start))
-
-(set-face-attribute 'default nil :height 140 :family "Operator Mono XLight")
 
 (org-babel-load-file "~/.emacs.d/emma.org")
 
