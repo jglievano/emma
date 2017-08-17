da8962b7ac3512cb3467dae1da0790fcd8fd2ec3
Gabe Lievano <gabe@jglievano.me>
2017-08-16 19:39:23 -0700

    Remove all .el

--- lisp/emma-syntax.el
+++
@@ -1,6 +0,0 @@
- (font-lock-add-keywords 'emacs-lisp-mode
-			'(("font-lock-add-keywords" . font-lock-keyword-face)
-			  ("provide-theme" . font-lock-keyword-face)
-			  ("load-theme" . font-lock-keyword-face)))
-
-(provide 'emma-syntax)
