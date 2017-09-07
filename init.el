8466077e6822b6b78fec53c42a5faa131ee0a7d3
Gabriel Lievano <gabriell@google.com>
2017-09-07 14:49:04 -0700

    Oversimplification of Doom.

--- init.el
+++ init.el
@@ -15,17 +15,34 @@
 ;;    ░   ░      ░   ░      ░     ░   ▒   
 ;;    ░  ░       ░          ░         ░  ░
 
-(defvar emma-version "1.1.0"
-  "Current Emma version.")
 
-(defvar emma-debug-mode (or (getenv "DEBUG") init-file-debug)
-  "If non-nil, all emma functions will be verbose. Set DEBUG=1 or use
---debug-init to enable.")
-                                       
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 (package-initialize)
 
-(load (expand-file-name "~/.emacs.d/old.el"))
+(defvar emma-version "2.0.1"
+  "Current Emma version.")
+
+(defvar emma-debug-mode (or (getenv "DEBUG") init-file-debug)
+  "If non-nil, all emma functions will be verbose. Set DEBUG=1 or use
+--debug-init to enable.")
+
+(defvar emma-emacs-dir (expand-file-name user-emacs-directory)
+  "The path to this emacs directory.")
+
+(defvar emma-core-dir (concat emma-emacs-dir "core/")
+  "Core files.")
+
+(defvar emma-modules-dir (concat emma-emacs-dir "modules/")
+  "Path to module directories.")
+
+(defvar emma-vendor-dir (concat emma-emacs-dir "vendor/")
+  "Vendor packages.")
+
+(defvar emma-local-dir (concat emma-emacs-dir ".local/")
+  "Local files.")
+
+;; Launch
+(require 'core (concat emma-core-dir "core"))
